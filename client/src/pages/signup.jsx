25a4c07e2476ce47504872a9dddf401a36637dfd
Abdul Salam Khan <abdulsalamkhan441@gmail.com>
2025-12-25 14:49:20 -0600

    a little change

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -21,7 +21,7 @@ export default function Signup() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://movion-backend.onrender.com", {
+      const res = await fetch("http://movion-backend.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
