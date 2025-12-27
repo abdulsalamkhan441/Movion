095d9e654342f25ac0344eebdd4783b64126a8e6
Abdul Salam Khan <abdulsalamk.k.19062@gmail.com>
2025-12-27 16:46:49 -0600

    Update signup.jsx

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -21,7 +21,7 @@ export default function Signup() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://movion-backend3.onrender.com/", {
+      const res = await fetch("https://movion-backend3.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
